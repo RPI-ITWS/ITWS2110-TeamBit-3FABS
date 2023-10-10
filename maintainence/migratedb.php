52cab9e912b4cbef30373e145a5c5cb051f398d6
PythonCoderAS <13932583+PythonCoderAS@users.noreply.github.com>
2023-10-10 15:07:21 -0400

    Add migration tech

--- maintainence/migratedb.php
+++ maintainence/migratedb.php
@@ -25,7 +25,13 @@
         } else {
             echo '<p class="success">Successfully connected to the database.</p>';
         }
-
+        
+        $db->query("CREATE TABLE IF NOT EXISTS metadata (id INT NOT NULL PRIMARY KEY, value VARCHAR(255) NOT NULL)");
+        $db->query("INSERT IGNORE INTO metadata (id, value) VALUES (1, '0')");
+        $migrationVersionResult = $db->query('SELECT value "migration_version" FROM metadata WHERE id = 1');
+        // Return result as integer
+        $migrationVersion = intval($migrationVersionResult->fetch_assoc()['migration_version']);
+        echo '<p class="success">Current migration version: ' . $migrationVersion . '</p>';
         ?> 
     </body>
 </html>
