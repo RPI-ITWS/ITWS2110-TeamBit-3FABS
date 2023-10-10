c64d9c19a43d41cde0dc399d293d8c16a63907a1
PythonCoderAS <13932583+PythonCoderAS@users.noreply.github.com>
2023-10-10 15:00:55 -0400

    Add success

--- maintainence/migratedb.php
+++ maintainence/migratedb.php
@@ -2,6 +2,17 @@
 <html>
     <head>
         <title>DB Migration</title>
+        <style>
+            .success {
+                color: green;
+            }
+            .failure {
+                color: red;
+            }
+            .warning {
+                color: orange;
+            }
+        </style>
     </head>
     <body>
         <h1>DB Migration</h1>
@@ -9,8 +20,10 @@
 
         $db = new mysqli("localhost","team5project","team5project","team5project");
         if ($db->connect_error) {
-            echo "<p>Connection failed: " . $db->connect_error . "</p>\n";
+            echo '<p class="failure">Connection failed: ' . $db->connect_error . "</p>\n";
             exit;
+        } else {
+            echo '<p class="success">Successfully connected to the database.</p>\n';
         }
 
         ?> 
