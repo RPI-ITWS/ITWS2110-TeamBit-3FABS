e0722ccfcccd9480fe31f0a3e4a2219835208922
PythonCoderAS <13932583+PythonCoderAS@users.noreply.github.com>
2023-10-10 20:58:27 -0400

    Make global

--- helpers/urls.php
+++ helpers/urls.php
@@ -1,6 +1,7 @@
 <?php
 require "config.php";
 function urlFor($script_path) {
+    global $root;
     // add the leading '/' if not present
     if($script_path[0] != '/') {
         $script_path = "/" . $script_path;
