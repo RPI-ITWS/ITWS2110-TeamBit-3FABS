e8c526375040c21b8963a52889758f421106339b
PythonCoderAS <13932583+PythonCoderAS@users.noreply.github.com>
2023-10-11 15:04:58 -0400

    Make browse work in PHP

--- helpers/urls.php
+++ helpers/urls.php
@@ -13,4 +13,10 @@ function urlFor($script_path) {
     }
     return $root . $script_path;
 }
+
+$assetURLs = array(
+    "style" => urlFor('/style.css'),
+    "heart" => urlFor('/images/heart.png'),
+    "liked" => urlFor('/images/heart-liked.png'),
+)
 ?>
