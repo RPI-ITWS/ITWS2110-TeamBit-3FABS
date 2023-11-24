71a8ab8e9e70ccfbe25052db6be396d4014cf844
KnightOkami <sullij14@rpi.edu>
2023-11-24 18:02:55 -0500

    Creation of log in
    Allows users to set up accounts

---
+++ create_acc.php
@@ -0,0 +1,140 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>1-Bit</title>
+    <link rel="stylesheet" href="./style.css">
+    <link rel="icon" href="./favicon.ico" type="image/x-icon">
+</head>
+
+<body>
+    <header>
+        <ul>
+            <li><a href="./index.php" class="navi selected">HOME</a></li>
+            <li><a href="./browse.html" class="navi">BROWSE</a></li>
+            <li><a href="./share.html" class="navi">SHARE</a></li>
+            <li><a href="./account.html" class="navi">LOGIN</a></li>
+        </ul>
+    </header>
+    <main class="content">
+        <section class="indexWrap">
+            <figure class="left">
+                <img src="./images/bit_logo_dither_trans.png" alt="1-Bit Logo">
+            </figure>
+            <article class="right">
+            <form action="create_acc.php" method="post">
+             
+             <p>
+                            <label for="firstName">First Name</label>
+                            <input type="text" name="first_name" id="firstName">
+                         </p>
+              
+                          
+             <p>
+                            <label for="lastName">Last Name</label>
+                            <input type="text" name="last_name" id="lastName">
+                         </p>
+              
+                          
+             <p>
+                            <label for="email">Email</label>
+                            <input type="text" name="email" id="email">
+                         </p>
+              
+                          
+             <p>
+                            <label for="username">Username</label>
+                            <input type="text" name="username" id="username">
+                         </p>
+              
+                          
+             <p>
+                            <label for="password">Password</label>
+                            <input type="text" name="password" id="password">
+                         </p>
+              
+                         <input type="submit" value="Submit">
+                      </form>
+            </article>
+           
+        </section>
+        <div id="accountMessage"></div>
+    </main>
+</body>
+
+
+    <?php if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        // servername => localhost
+        // username => root
+        // password => empty
+        // database name => staff
+        $conn = mysqli_connect("localhost", "root", "", "project");
+
+        // Check connection
+        if ($conn === false) {
+            die("ERROR: Could not connect. " . mysqli_connect_error());
+        }
+
+        // Taking all 5 values from the form data(input)
+        $id = null;
+        $fname = mysqli_real_escape_string($conn, $_REQUEST["first_name"]);
+        $lname = mysqli_real_escape_string($conn, $_REQUEST["last_name"]);
+        $email = mysqli_real_escape_string($conn, $_REQUEST["email"]);
+        $user_name = mysqli_real_escape_string($conn, $_REQUEST["username"]);
+        $password = mysqli_real_escape_string($conn, $_REQUEST["password"]);
+        $salt = bin2hex(random_bytes(16));
+
+        // Ensure the usernmae they wish to use is free
+        $stmt = $conn->prepare("SELECT * FROM users WHERE user_name = ?");
+        $stmt->bind_param("s", $user_name);
+        $stmt->execute();
+        $result = $stmt->get_result();  
+
+        if (mysqli_num_rows($result) > 0) {
+            echo '<script>document.getElementById("accountMessage").innerHTML = "This username is already in use!";</script>';
+            mysqli_close($conn);
+        }
+        else{
+            //Ensure email hasnt been used already
+            $stmt = $conn->prepare("SELECT * FROM users WHERE email = ?");
+            $stmt->bind_param("s", $email);
+            $stmt->execute();
+            $result = $stmt->get_result();  
+
+            if (mysqli_num_rows($result) > 0) {
+                echo '<script>document.getElementById("accountMessage").innerHTML = "This email is already associated to an account!";</script>';
+                mysqli_close($conn);
+                
+                
+            } else {
+
+
+
+
+                $salted =$salt.$password;
+
+                $hashed = hash('sha512', $salted);
+                $sql = "INSERT INTO users (id, fname, lname, user_name, password, email, salt) 
+                VALUES (NULL, ?, ?, ?, ?, ?, ?)";
+                $stmt = mysqli_prepare($conn, $sql);
+                if ($stmt) {
+                    mysqli_stmt_bind_param($stmt, "ssssss", $fname, $lname, $user_name, $hashed, $email, $salt);
+                    if (mysqli_stmt_execute($stmt)) {
+                        echo '<script>document.getElementById("accountMessage").innerHTML = "Account Created Successfully!";</script>';
+                    } else {
+                        echo "ERROR: Could not execute $sql. " . mysqli_error($conn);
+                    }
+                    mysqli_stmt_close($stmt);
+                } else {
+                    echo "ERROR: Could not prepare statement. " . mysqli_error($conn);
+                }
+            }
+        }
+    } ?>
+
+
+    
+</html>
---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>1-Bit</title>
+    <link rel="stylesheet" href="./style.css">
+    <link rel="icon" href="./favicon.ico" type="image/x-icon">
+</head>
+
+<body>
+    <header>
+        <ul>
+            <li><a href="./index.php" class="navi selected">HOME</a></li>
+            <li><a href="./browse.html" class="navi">BROWSE</a></li>
+            <li><a href="./share.html" class="navi">SHARE</a></li>
+            <li><a href="./account.html" class="navi">LOGIN</a></li>
+        </ul>
+    </header>
+    <main class="content">
+        <section class="indexWrap">
+            <figure class="left">
+                <img src="./images/3fabs.png" alt="1-Bit Logo">
+            </figure>
+            <article class="right">
+                <h1>Join the dither revolution.</h1>
+                <p>Log in, or create a new account.</p>
+                <div class="buttonWrap">
+                    <button class="login" onclick="location.href = './login.php'"><h2>Log In</h2></button>
+                    <button class="signup" onclick="location.href = './create_acc.php'"><h2>Create Account</h2></button>
+                </div>
+            </article>
+        </section>
+    </main>
+</body>
+</html>
