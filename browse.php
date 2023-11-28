84afa7a90cfc56ce4e97c3d4ca9e69958221da73
PythonCoderAS <13932583+PythonCoderAS@users.noreply.github.com>
2023-11-28 13:35:19 -0500

    Modify browse page

--- browse.php
+++ browse.php
@@ -1,36 +1,18 @@
-<!DOCTYPE html>
-<html lang="en">
-<?php require './helpers/urls.php' ?>
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title><?php echo $_GET["sort"] . ' Posts' ?></title>
-    <link rel="stylesheet" href="<?php echo urlFor('/style.css') ?>">
-    <!-- <link rel="icon" href="./favicon.ico" type="image/x-icon"> -->
-    <link rel="icon" href="<?php echo urlFor('/images/favicon.ico') ?>" type="image/x-icon">
-</head>
-<body>
-    <header>
-        <ul>
-            <li><a href="<?php echo urlFor('/') ?>" class="navi">HOME</a></li>
-            <li><a href="<?php echo urlFor('/browse') ?>" class="navi selected">BROWSE</a></li>
-            <li><a href="<?php echo urlFor('/share') ?>" class="navi">SHARE</a></li>
-            <li><a href="<?php echo urlFor('/login') ?>" class="navi">LOGIN</a></li>
-        </ul>
-    </header>
-    <div class="contentFeed">
-        <p>There should be a feed of content here, wouldnt be suprised if this becomes browse.php. You should also be able to switch between friends and general feeds</p>
-        <?php
-        require_once './helpers/db.php';
-        global $db;
-        global $assetURLs;
-        $sort = $_GET["sort"];
-        // $time = $_GET["time"]; Not used yet
-        $sortMode = ($_GET["sortMode"] ?? "desc") == "desc" ? "desc" : "asc";
-        $limit = 50;
-        $sortColumn = "post_created_at";
-        $preparedPostQuery = $db->prepare('
+<?php
+require './helpers/heading.php';
+generate_header();
+?>
+<div class="contentFeed">
+    <?php
+    require_once './helpers/db.php';
+    global $db;
+    global $assetURLs;
+    $sort = $_GET["sort"];
+    // $time = $_GET["time"]; Not used yet
+    $sortMode = ($_GET["sortMode"] ?? "desc") == "desc" ? "desc" : "asc";
+    $limit = 50;
+    $sortColumn = "post_created_at";
+    $preparedPostQuery = $db->prepare('
             SELECT
                 posts.id AS "post_id",
                 image_url,
@@ -64,50 +46,50 @@
                 GROUP BY comments.post_id
             ) num_comments_subquery ON num_comments_subquery.comment_post_id = post_id
             ORDER BY :sortColumn ' . $sortMode . '
-            LIMIT '. $limit .';
+            LIMIT ' . $limit . ';
         ');
-        // Look, I know we're not supposed to do this but I cannot find a better way to get PHP to stop yelling at me about both the sort direction (asc/desc) or the limit
+    // Look, I know we're not supposed to do this but I cannot find a better way to get PHP to stop yelling at me about both the sort direction (asc/desc) or the limit
 
-        switch ($sort) {
-            case "new":
-                $sortColumn = "post_created_at";
-                break;
-            case "top":
-                $sortColumn = "num_likes";
-                break;
-            default:
-                $sortColumn = "post_created_at";
-                break;
-        }
+    switch ($sort) {
+        case "new":
+            $sortColumn = "post_created_at";
+            break;
+        case "top":
+            $sortColumn = "num_likes";
+            break;
+        default:
+            $sortColumn = "post_created_at";
+            break;
+    }
 
-//        $preparedPostQuery->bind_param("si", $sortColumn, $limit);
-        $preparedPostQuery->execute(["sortColumn" => $sortColumn]);
-        $posts = $preparedPostQuery->fetchAll(PDO::FETCH_ASSOC);
-        $preparedPostQuery->closeCursor();
-        ?>
+    //        $preparedPostQuery->bind_param("si", $sortColumn, $limit);
+    $preparedPostQuery->execute(["sortColumn" => $sortColumn]);
+    $posts = $preparedPostQuery->fetchAll(PDO::FETCH_ASSOC);
+    $preparedPostQuery->closeCursor();
+    ?>
 
-        <?php 
-        foreach ($posts as $post) {
-            $altText = $post["alt_text"] ?? ($post["title"] . " by " . $post["display_name"]);
-            echo '<div class="post">
-                <h1 class="postTitle">'. $post["title"] . '</h1>
-                <img class="postImage" src="'. urlFor('/images/'. $post['image_url']) .'" alt="'. $altText . '">
+    <?php
+    foreach ($posts as $post) {
+        $altText = $post["alt_text"] ?? ($post["title"] . " by " . $post["display_name"]);
+        echo '<div class="post">
+                <h1 class="postTitle">' . $post["title"] . '</h1>
+                <img class="postImage" src="' . urlFor('/images/' . $post['image_url']) . '" alt="' . $altText . '">
                 <div class="postDetails">
-                    <p class="author"><a href="'. urlFor('/profile/' . $post['username']) .'">'. $post['display_name'] .'</a></p>
-                    <p class="postDate">'. formatDate($post["post_created_at"]) .'</p>
-                    <p class="likes">Likes: '. $post["num_likes"] .'</p>
-                    <p class="comments">Comments: '. $post["num_comments"] .'</p>
+                    <p class="author"><a href="' . urlFor('/profile/' . $post['username']) . '">' . $post['display_name'] . '</a></p>
+                    <p class="postDate">' . $post["post_created_at"] . '</p>
+                    <p class="likes">Likes: ' . $post["num_likes"] . '</p>
+                    <p class="comments">Comments: ' . $post["num_comments"] . '</p>
                 </div>
                 <div class="postFooter">
-                    <img class="like" src="'. $assetURLs['heart'] .'" onclick="like(this)" alt="Like button">
+                    <img class="like" src="' . $assetURLs['heart'] . '" onclick="like(this)" alt="Like button">
                 </div>
             </div>';
-        }
-        ?>
+    }
+    ?>
 
-        
 
-        <!-- <div class="post">
+
+    <!-- <div class="post">
             <h1 class="postTitle">End of the Dither-Day</h1>
             <img class="postImage" src="./images/sunset.png">
             <div class="postFooter ">
@@ -143,8 +125,6 @@
                 <p class="postDate">08/31/2018</p>
             </div>
         </div> -->
-    </div>
-    <script src="<?php urlFor('/Javascript/Functions.js') ?>"></script>
-</body>
-
-</html>
+</div>
+<script src="<?php urlFor('/Javascript/Functions.js') ?>"></script>
+<?php generate_footer(); ?>
