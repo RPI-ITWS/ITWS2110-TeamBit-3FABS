c1fa0bb8abdefc3c6b1c81358779f6c3d7f88230
PythonCoderAS <13932583+PythonCoderAS@users.noreply.github.com>
2023-11-28 13:45:08 -0500

    work?

--- helpers/db.php
+++ helpers/db.php
@@ -1,7 +1,4 @@
 <?php
-ini_set('display_errors', '1');
-ini_set('display_startup_errors', '1');
-error_reporting(E_ALL);
 $db = new PDO('mysql:host=localhost;dbname=team5project', 'team5project', 'team5project');
 $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 ?>
