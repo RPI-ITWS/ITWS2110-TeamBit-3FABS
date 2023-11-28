d8451380fc968d3644ba0dc873e193762922480e
KnightOkami <sullij14@rpi.edu>
2023-11-28 12:57:08 -0500

    Update index.php
    Fix broken links in index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
-
+<?php require './helpers/urls.php' ?>
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -28,6 +28,9 @@
                 <h1>Join the dither revolution.</h1>
                 <p>Log in, or create a new account.</p>
                 <div class="buttonWrap">
+                <?php         ini_set('display_errors', '1');
+        ini_set('display_startup_errors', '1');
+        error_reporting(E_ALL); ?>
                 <button class="login" onclick="location.href = '<?php echo urlFor('/login.php') ?>'"><h2>Log In</h2></button>
                 <button class="signup" onclick="location.href = '<?php echo urlFor('/create_acc.php') ?>'"><h2>Create Account</h2></button>
                 </div>
