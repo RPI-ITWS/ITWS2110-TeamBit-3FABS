b7fd1c712aff5f3abbc1283f8f898c4a0893c915
Andrei Dudau <dudaua@rpi.edu>
2023-12-06 15:15:55 -0500

    Making login required to post

--- helpers/heading.php
+++ helpers/heading.php
@@ -9,10 +9,15 @@ function generate_header(string $title = "TeamBit-3FABS") {
     global $userInfo;
     $rootURL = urlFor('/');
     $browseURL = urlFor('/browse');
-    $shareURL = urlFor('/share.html');
+    $shareURL = "";
     $loginURL = urlFor('/login');
     $accountCreationURL = urlFor('/create_acc');
     $loggedInUser = getCurrentUserInfo();
+    if ($loggedInUser !== null) {
+        $shareURl = urlFor('/share.html');
+    } else {
+        $shareURL = $shareURl = urlFor('/login');
+    }
     if ($loggedInUser === null && ($_SERVER['REQUEST_URI'] !== $loginURL || $_SERVER['REQUEST_URI'] !== $accountCreationURL)) {
         // Make it easier for users to login
         $_SESSION['login_redirect'] = $_SERVER['REQUEST_URI'];
